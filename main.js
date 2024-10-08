d70453a6d7e3b3ac1e5685959d160fb6123b3160
Prof-RafaGrillo <125394387+Prof-RafaGrillo@users.noreply.github.com>
2024-10-08 08:49:31 -0300

    .

--- main.js
+++ main.js
@@ -2,6 +2,14 @@ function  criaCartao(pergunta, resposta){
     let conteudo = document.getElementById('conteudo') 
     let cartao = document.createElement('article')
     cartao.className = 'cartao'
+    
+    cartao.innerHTML = `
+    <div class="cartao--conteudo">
+    <div class="cartao--pergunta"><p>Quando foi lançado o One Piece?</p></div>
+    <div class="cartao--resposta"><p>Em 1997</p></div>
+    </div>
+    `
     console.log(cartao)
+
 }
 
