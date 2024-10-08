20090cb928744fc9adaa4ac84febc10f5f05cc09
Prof-RafaGrillo <125394387+Prof-RafaGrillo@users.noreply.github.com>
2024-10-08 08:22:27 -0300

    .

--- main.js
+++ main.js
@@ -2,4 +2,3 @@ function  criaCartao(pergunta, resposta){
     console.log(pergunta, resposta)
 }
 
-criaCartao(1,2)
--- pergunta.js
+++ pergunta.js
@@ -2,3 +2,11 @@ criaCartao(
     "Quando foi lançado o One Piece?",
     "Em 1997"
 )
+criaCartao(
+    "Quando foi lançado o One Piece?",
+    "Em 1997"
+)
+criaCartao(
+    "Quando foi lançado o One Piece?",
+    "Em 1997"
+)
